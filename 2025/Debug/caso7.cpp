82cdce1c7ffeec78de3415c04512ea863bec91fb
Hector Spataro <hector.spataro@ipm.edu.ar>
2025-11-05 08:27:33 -0300

    Una mas

--- 2025/Debug/caso7.cpp
+++ 2025/Debug/caso7.cpp
@@ -8,6 +8,8 @@ using namespace std;
 #define VUELTAS 10
 #define INICIAL 100
 
+enum OPCIONES {OPC1, OP2, OP3};
+
 struct completa_t
 {
     int campo1;
@@ -36,7 +38,14 @@ int main (void)
     completa_t misDatos[10];
     completa_t aux;
     vector <completa_t> larguito;
+    char * palabra = "DaleQueVa";
+    OPCIONES enumerando;
 
+    int miarray[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    vector <int> mivector;
+
+    enumerando = OPC1;
+    enumerando = (OPCIONES) 9;
     archivoL.open("caso6.cpp");
 
     if(archivoL.is_open())
@@ -46,6 +55,7 @@ int main (void)
             misDatos[indice] = cargar (indice);
             aux = cargar (indice+indice);
             larguito.push_back(aux);
+            mivector.push_back(indice+1);
         }
     }
     return 0;
@@ -57,6 +67,8 @@ Ver una variable file
 Ver una variable estructura
 Ver un vector y un array
 Poner breakpoints
+Poner mas de un breakpoint y moverse a lo pavo.
+
 Ver posiciones de memoria
 
 
@@ -69,11 +81,14 @@ Pila de llamada a funciones
 Variables globales
 Variables estáticas
 
-Agregar datos en inspección. Variales, direcciones de memoria, punteros e incluso metodos
+Agregar datos en inspección: Variables, direcciones de memoria, punteros e incluso metodos
 
 Mostrar como desaparecen las variables en locales, o en inspección cuando me voy de la función
 
 
-Define y enum
+Define y enum NOOOOOOO
+
+Diferencia en memoria de como guarda un vector o un array
+Y un string?
 
 */
